b54f1f3cd087de30edbcc20157c56099d9568214
oblessnoob <dongduoduo2011@126.com>
2025-07-28 04:19:56 +0900

    Add Android and display platform

--- src/main.py
+++ src/main.py
@@ -2,20 +2,23 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
+    match page.platform:
+        case ft.PagePlatform.WINDOWS:
+            platform = ft.Text("Windows")
+        case ft.PagePlatform.LINUX:
+            platform = ft.Text("Linux")
+        case ft.PagePlatform.MACOS:
+            platform = ft.Text("MacOS")
+        case ft.PagePlatform.ANDROID:
+            platform = ft.Text("Android")
+        case ft.PagePlatform.ANDROID_TV:
+            platform = ft.Text("Android TV")
+        case ft.PagePlatform.IOS:
+            platform = ft.Text("iOS")
     page.add(
         ft.SafeArea(
             ft.Container(
-                counter,
+                platform,
                 alignment=ft.alignment.center,
             ),
             expand=True,
